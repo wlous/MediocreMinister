d207f5995f4c272c623b9ae4f424a5483b47f520
louise <louise.ahokas1@ingka.ikea.com>
2022-05-21 18:43:03 +0200

    Initial Commit

---
+++ MediocreMinister/MediocreMinisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediocreMinisterApp.swift
+//  MediocreMinister
+//
+//  Created by Louise Ahokas on 2022-05-21.
+//
+
+import SwiftUI
+
+@main
+struct MediocreMinisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
