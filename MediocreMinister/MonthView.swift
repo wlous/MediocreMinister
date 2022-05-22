ff074c2cc93d2235faf3b96e52e60bef6c377fc8
louise <louiseahokas@gmail.com>
2022-05-22 10:09:25 +0200

    Added ScrollView

--- MediocreMinister/MonthView.swift
+++ MediocreMinister/MonthView.swift
@@ -2,27 +2,40 @@ import SwiftUI
 
 struct MonthView: View {
     var body: some View {
-        Button(action: {
-           print("Delete tapped!")
-       }) {
-           HStack {
-               Image(systemName: "plus")
-                   .font(.title3)
-//                       Text("Add")
-//                           .font(.title3)
-           }
-           .frame(minWidth: 0, maxWidth: 327)
-           .padding(20)
-           .foregroundColor(Color.black)
-           .background(Color.theme.background)
-           .cornerRadius(24)
-//                   .buttonStyle(.bordered)
-           .overlay(
-               RoundedRectangle(cornerRadius: 24)
-                .stroke(style: StrokeStyle(lineWidth: 1, dash: [4]))
-               )
-           
-       }
+        ScrollView(.horizontal, showsIndicators: true) {
+            HStack(spacing:20) {
+                ForEach(0..<12) {_ in
+                    VStack {
+                        Text("Videos")
+                        Text("Placement")
+                        Text("Bible Studies")
+                        Text("Return Visits")
+                        Text("Hours")
+                        Button(action: {
+                           print("Delete tapped!")
+                       }) {
+                           HStack {
+                               Image(systemName: "plus")
+                                   .font(.title3)
+                //                       Text("Add")
+                //                           .font(.title3)
+                           }
+                           .frame(minWidth: 0, maxWidth: 327)
+                           .padding(20)
+                           .foregroundColor(Color.black)
+                           .background(Color.theme.background)
+                           .cornerRadius(24)
+                //                   .buttonStyle(.bordered)
+                           .overlay(
+                               RoundedRectangle(cornerRadius: 24)
+                                .stroke(style: StrokeStyle(lineWidth: 1, dash: [4]))
+                               )
+                           
+                   }
+                }
+                }
+            }
+        }
     }
 }
 
